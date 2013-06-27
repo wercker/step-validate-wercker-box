d16b1eccc9fb9686fbb65ae2a2c028b5195c06d4
wwwouter <wouter@wercker.com>
2013-06-27 12:34:56 +0200

    first

---
+++ validator.js
@@ -0,0 +1,32 @@
+var jsYaml = require("js-yaml")
+var fs = require("fs")
+var path = require("path")
+var JSV = require("JSV").JSV
+
+
+var werckerBoxSchema = JSON.parse(fs.readFileSync(path.join(__dirname, 'wercker-box-schema.json')).toString())
+
+exports.validate = function (filename, callback) {
+
+  if(!fs.existsSync(filename)){
+    return callback("File " + filename + " does not exist.")
+  }
+
+  var yamlContents = fs.readFileSync(filename).toString();
+  var yaml;
+  try{
+    yaml = jsYaml.load(yamlContents);
+  }
+  catch(error){
+    return callback(error)
+  }
+
+  var env = JSV.createEnvironment()
+  var report = env.validate(yaml, werckerBoxSchema)
+
+  if(report.errors.length === 0){
+    return callback(null)
+  }
+
+  return callback(report.errors)
+}
